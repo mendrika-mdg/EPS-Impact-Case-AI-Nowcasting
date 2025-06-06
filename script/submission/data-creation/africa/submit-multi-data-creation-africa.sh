37079706a879b150ae40d6130c3ee11ba28ee066
mendrika-hpc-jasmin <mmmhr@leeds.ac.uk>
2025-06-06 15:55:55 +0100

    Corrected relative SHAP

--- script/submission/data-creation/africa/submit-multi-data-creation-africa.sh
+++ script/submission/data-creation/africa/submit-multi-data-creation-africa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for year in 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023; do
+for year in 2017 2021 2022 2023; do
     sbatch --job-name=a$year /home/users/mendrika/EPS-Impact-Case-AI-Nowcasting/script/submission/data-creation/africa/data-creation-africa.sh $year
 done
